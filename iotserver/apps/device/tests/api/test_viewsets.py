82699afa226b5d95785488091f99693e678c93e1
Jonathan Bydendyk <jpbydendyk@gmail.com>
2021-10-05 21:40:12 +0200

    Added travis config

--- iotserver/apps/device/tests/api/test_viewsets.py
+++ iotserver/apps/device/tests/api/test_viewsets.py
@@ -64,7 +64,7 @@ class TestLocationViewset(object):
         )
         return_data = response.json()
 
-        assert response.status_code == 200, response.json()
+        assert response.status_code == 200
         assert return_data['name'] == location_data['name']
         assert return_data['position'] == location_data['position']
 
@@ -109,6 +109,81 @@ class TestDeviceTypeViewset(object):
         )
         return_data = response.json()
 
-        assert response.status_code == 200, response.json()
+        assert response.status_code == 200
         assert return_data['name'] == device_type_data['name']
         assert str(device_type.pk) in return_data['url']
+
+
+@pytest.mark.django_db
+class TestDeviceViewset(object):
+    root_url = '/api/devices/'
+
+    def test_list(self, api_client):
+        device = device_factories.DeviceFactory()
+        response = api_client.get(self.root_url)
+        return_data = response.json()
+
+        assert response.status_code == 200
+        assert len(return_data) == 1
+
+        assert return_data[0]['name'] == device.name
+
+        assert str(device.pk) in return_data[0]['url']
+        assert str(device.type.pk) in return_data[0]['type']
+        assert str(device.location.pk) in return_data[0]['location']
+
+    def test_detail(self, api_client):
+        device = device_factories.DeviceFactory()
+        response = api_client.get(f'{self.root_url}{device.pk}/')
+        return_data = response.json()
+
+        assert response.status_code == 200
+        assert return_data['name'] == device.name
+
+        assert str(device.pk) in return_data['url']
+        assert str(device.type.pk) in return_data['type']
+        assert str(device.location.pk) in return_data['location']
+
+    def test_create(self, api_client):
+        device_type = device_factories.DeviceTypeFactory()
+        location = device_factories.LocationFactory()
+        device_data = {
+            'name': 'Test Device',
+            'description': 'Test Device Description',
+            'type': device_type.pk,
+            'location': location.pk,
+            'ip_address': '192.168.0.2',
+            'mac_address': 'EE:01:A0:01:60:BE',
+            'hostname': 'test-device-002',
+        }
+        response = api_client.post(f'{self.root_url}', device_data, format='json')
+        return_data = response.json()
+
+        assert response.status_code == 201
+        assert return_data['name'] == device_data['name']
+
+        assert str(device_type.pk) in return_data['type']
+        assert str(location.pk) in return_data['location']
+
+    def test_update(self, api_client):
+        device = device_factories.DeviceFactory()
+        device_data = {
+            'name': 'Test Device',
+            'description': 'Test Device Description',
+            'type': device.type.pk,
+            'location': device.location.pk,
+            'ip_address': device.ip_address,
+            'mac_address': device.mac_address,
+            'hostname': device.hostname,
+        }
+        response = api_client.put(
+            f'{self.root_url}{device.pk}/', device_data, format='json'
+        )
+        return_data = response.json()
+
+        assert response.status_code == 200, return_data
+        assert return_data['name'] == device_data['name']
+
+        assert str(device.pk) in return_data['url']
+        assert str(device.type.pk) in return_data['type']
+        assert str(device.location.pk) in return_data['location']
