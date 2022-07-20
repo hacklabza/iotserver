3c8c9ffc9d1719929d2f6b557d7b86bdd56e74e4
Jonathan Bydendyk <jpbydendyk@gmail.com>
2022-07-20 21:07:07 +0200

    Increased cache timeout

--- iotserver/settings.py
+++ iotserver/settings.py
@@ -92,7 +92,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
         'LOCATION': '127.0.0.1:11211',
-        'TIMEOUT': (60 * 15),  # 15 mins
+        'TIMEOUT': (60 * 60),  # 1 hour
     }
 }
 
