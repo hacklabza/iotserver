5e4a0d9e6621823d79642c1c7f719d63cf2edaa8
Jonathan Bydendyk <jonathan@hacklab.co.za>
2025-10-28 21:34:05 +0200

    Improved aggregation averages

--- iotserver/apps/device/utils/stats.py
+++ iotserver/apps/device/utils/stats.py
@@ -9,17 +9,21 @@ def aggregate_statuses(statuses):
     statuses = statuses.filter(created_at__date=today)
 
     aggregates = {}
+    values_for_avg = {}
+
     for status in statuses:
         for key, value in status.status.items():
             if isinstance(value, dict):
                 if key not in aggregates:
                     aggregates[key] = {}
+                    values_for_avg[key] = {}
                 for subkey, subvalue in value.items():
                     if subkey not in aggregates[key]:
                         aggregates[key][subkey] = {
                             'minimum': subvalue,
                             'maximum': subvalue
                         }
+                        values_for_avg[key][subkey] = [subvalue]
                     else:
                         aggregates[key][subkey]['minimum'] = min(
                             aggregates[key][subkey]['minimum'], subvalue
@@ -27,17 +31,18 @@ def aggregate_statuses(statuses):
                         aggregates[key][subkey]['maximum'] = max(
                             aggregates[key][subkey]['maximum'], subvalue
                         )
+                        values_for_avg[key][subkey].append(subvalue)
                     aggregates[key][subkey]['average'] = (
-                        aggregates[key][subkey]['minimum'] +
-                        aggregates[key][subkey]['maximum']
-                    ) / 2
+                        sum(values_for_avg[key][subkey]) / len(values_for_avg[key][subkey])
+                    )
             else:
                 if key not in aggregates:
                     aggregates[key] = {'minimum': value, 'maximum': value}
+                    values_for_avg[key] = [value]
                 else:
                     aggregates[key]['minimum'] = min(aggregates[key]['minimum'], value)
                     aggregates[key]['maximum'] = max(aggregates[key]['maximum'], value)
-                aggregates[key]['average'] = (
-                    aggregates[key]['minimum'] + aggregates[key]['maximum']
-                ) / 2
+                    values_for_avg[key].append(value)
+                aggregates[key]['average'] = sum(values_for_avg[key]) / len(values_for_avg[key])
+
     return aggregates
