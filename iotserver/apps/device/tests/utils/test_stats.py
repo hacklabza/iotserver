a57dcb0242d9ce90ebe1d6d8beab6be42d820030
Jonathan Bydendyk <jonathan@hacklab.co.za>
2025-11-18 06:30:38 +0200

    Added initial unittests for utils

---
+++ iotserver/apps/device/tests/utils/test_stats.py
@@ -0,0 +1,40 @@
+import pytest
+
+from iotserver.apps.device.tests import factories as device_factories
+
+
+@pytest.mark.django_db
+class TestAggregateStatuses(object):
+    def setup_method(self, test_method):
+        self.location = device_factories.DeviceFactory()
+
+    def test_aggregate_statuses(self):
+        statuses = [
+            device_factories.DeviceStatusFactory(
+                device=self.location,
+                status={
+                    'sensor-a': {'reading-a': 30, 'reading-b': 15},
+                    'sensor-b': 20,
+                    'sensor-c': True,
+                },
+            ),
+            device_factories.DeviceStatusFactory(
+                device=self.location,
+                status={
+                    'sensor-a': {'reading-a': 30, 'reading-b': 15},
+                    'sensor-b': 40,
+                    'sensor-c': False,
+                },
+            ),
+        ]
+
+        aggregate = self.location.aggregate_statuses
+
+        assert aggregate == {
+            'sensor-a': {
+                'reading-a': {'minimum': 30, 'maximum': 30, 'average': 30.0},
+                'reading-b': {'minimum': 15, 'maximum': 15, 'average': 15.0},
+            },
+            'sensor-b': {'minimum': 20, 'maximum': 40, 'average': 30.0},
+            'sensor-c': None,
+        }
